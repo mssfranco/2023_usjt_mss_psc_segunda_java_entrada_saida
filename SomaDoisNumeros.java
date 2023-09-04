04df80e0e84c6307532539824c04b582208748c5
Moises Rafael <moisesfranco.8231@aluno.saojudas.br>
2023-09-04 19:38:34 -0300

    ap20(input-output): delcaracao de variaveis

---
+++ SomaDoisNumeros.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SomaDoisNumeros{
+    public static void main(String [] args){
+        //declarando variável
+        Scanner inputUsuario = new Scanner(System.in);
+
+        double v1;
+        double v2;
+        double resultado;
+        
+        //entrada
+        
+
+        //processamento
+
+
+        //saida
+    }
+}
